5cb8f5d4150d32360a45f80750de3f2c690a5ad4
Diana Mounter <broccolini@github.com>
2019-06-23 00:31:31 -0400

    move toggle into header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,36 @@
 import React from 'react'
+import { useColorMode } from 'theme-ui'
 import {
+  Button,
   Flex,
+  Heading,
   Link,
   StyledOcticon,
 } from '@primer/components'
-import {MarkGithub} from '@githubprimer/octicons-react'
+import {MarkGithub, LightBulb} from '@githubprimer/octicons-react'
 
-export default props =>
+export default props => {
+  const [ mode, setMode ] = useColorMode()
 
-<Flex bg="black" color="white" p={3}>
-  <StyledOcticon color="white" icon={MarkGithub} size="28" mr={3}/>
-  <Flex.Item alignItems="center">
-    <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Pull Requests</Link>
-    <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Issues</Link>
-    <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Marketplace</Link>
-    <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Explore</Link>
-  </Flex.Item>
-</Flex>
+  const toggleMode = () => {
+    setMode(mode === 'dark' ? 'light' : 'dark')
+  }
+
+  return (
+    <Flex bg="black" color="white" p={3}>
+      <StyledOcticon color="white" icon={MarkGithub} size="28" mr={3}/>
+      <Flex.Item alignItems="center" width={2/3}>
+        <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Pull Requests</Link>
+        <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Issues</Link>
+        <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Marketplace</Link>
+        <Link px="2" fontSize="1" fontWeight="bold" color="white" href="">Explore</Link>
+      </Flex.Item>
+      <Flex justifyContent="flex-end" width={1/3}>
+        <Button onClick={toggleMode}>
+        <StyledOcticon color="gray.9" icon={LightBulb} size="16" mr={1}/>
+          {mode}
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,24 @@
 import React from 'react'
-import { useColorMode } from 'theme-ui'
 import {
   Avatar,
   Box,
   Button,
   BaseStyles,
   Flex,
+  Heading,
 } from '@primer/components'
 import Header from '../components/Header.js'
 import Overview from '../components/Overview.js'
 import Bio from '../components/Bio.js'
 
 
-export default props => {
-  const [ mode, setMode ] = useColorMode()
-
-  const toggleMode = () => {
-    setMode(mode === 'dark' ? 'light' : 'dark')
-  }
-
-  return (
-    <Box>
-      <h1>Hello Potato {mode}</h1>
-      <Button onClick={toggleMode}>
-        {mode}
-      </Button>
-      <BaseStyles>
-        <Header />
-        <Flex maxWidth="1280px" px={3} mt={6} mx={'auto'}>
-          <Bio />
-          <Overview />
-        </Flex>
-      </BaseStyles>
-    </Box>
-  )
-}
+export default props => 
+  <Box>
+    <BaseStyles>
+      <Header />
+      <Flex maxWidth="1280px" px={3} mt={6} mx={'auto'}>
+        <Bio />
+        <Overview />
+      </Flex>
+    </BaseStyles>
+  </Box>
