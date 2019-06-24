4cc24436e73c57c58f408f3972c7bfc18ce71384
Diana Mounter <broccolini@github.com>
2019-06-23 22:34:56 -0400

    tweaks

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -13,14 +13,14 @@ import Pins from './Pins.js'
 import Contributions from './Contributions.js'
 
 export default props =>
-<Box width={3/4} pl={2} px={2}>
+<Box width={3/4} pl={2} pl={2}>
   <UnderlineNav>
     <UnderlineNav.Link href="#foo" selected>
       Overview
     </UnderlineNav.Link>
     <UnderlineNav.Link href="#bar">Repositories <CounterLabel>74</CounterLabel></UnderlineNav.Link>
     <UnderlineNav.Link href="#baz">Projects <CounterLabel>0</CounterLabel></UnderlineNav.Link>
-    <UnderlineNav.Link href="#baz">Stars <CounterLabel>407</CounterLabel></UnderlineNav.Link>
+    <UnderlineNav.Link href="#baz">Stars <CounterLabel>406</CounterLabel></UnderlineNav.Link>
     <UnderlineNav.Link href="#baz">Followers <CounterLabel>1.1k</CounterLabel></UnderlineNav.Link>
     <UnderlineNav.Link href="#baz">Following <CounterLabel>114</CounterLabel></UnderlineNav.Link>
   </UnderlineNav>
--- src/components/PinItem.js
+++ src/components/PinItem.js
@@ -19,12 +19,12 @@ const PinItem = ({
 }) => (
   <Box width={1/2} px={2} py={2}>
 
-    <BorderBox p={3} >
+    <BorderBox p={3} borderColor="gray.3">
       <Flex alignItems="center">
         <StyledOcticon icon={Repo} size="16" color="gray.6" mr={2} />
-        <Link fontWeight="bold" display="block" href="">{name}</Link>
+        <Link fontWeight="bold" display="block" fontSize={1} lineHeigh={1} href="">{name}</Link>
       </Flex>
-      <Text as="p" fontSize={0} color="gray.5">{desc}</Text>
+      <Text as="p" mt={2} fontSize={0} color="gray.5">{desc}</Text>
       <Flex flexDirection="row">
         <Flex mr={4} alignItems="center">
           <StyledOcticon icon={PrimitiveDot} size="24" color={languagecolor} mr={1} />
